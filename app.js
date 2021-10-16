b5fb00dcdb63882192198abd2c170b4ae8243316
ntzwilly <ntzwilly@gmail.com>
2021-10-16 22:14:57 +0200

    Remove ++

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('#container');
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
-for(let i = 1; i <= 871; i++){
+for(let i = 1; i <= 871; i += 1){
   const pokemon = document.createElement('div');
   pokemon.classList.add('pokemon');
   const label = document.createElement('span');
